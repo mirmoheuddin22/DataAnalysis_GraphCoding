dc0c35c454cbe2c1443e58387918513e58effc79
Mir Md. Moheuddin <115165307+mirmoheuddin22@users.noreply.github.com>
2022-11-01 00:06:34 +0100

    fifa data graph in python

---
+++ Fifa data graph.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+fifa = pd.read_csv(r"D:\Data set\fifa_data.csv")
+print(fifa.head())
+#Histograms:
+bins = [40, 50, 60, 70, 80, 90, 100]
+bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+plt.hist(fifa.Overall, bins = bins, color = '#abcdef')
+plt.xticks(bins)
+plt.xlabel('Number of Players')
+plt.ylabel('Skill Level')
+plt.title('Distribution of Player Skills in FIFA 2018')
+plt.show()
+#Pie Charts:
+left = fifa.loc[fifa['Preferred Foot'] == 'Left'].count()[0]
+right = fifa.loc[fifa['Preferred Foot'] == 'Right'].count()[0]
+labels = ['Left', 'Right']
+colors = ['#abcdef', '#aabbcc']
+plt.pie([left, right], labels = labels, colors=colors, autopct='%.2f')
+plt.show()
+print(left)
